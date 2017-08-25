b3fb0cddb19357aa87a9ea9eb84f6e5bb780d132
Michael Oliver <michael.d.oliver@gmail.com>
2017-08-25 10:49:57 -0700

    add imports so it installs properly

--- kfs/__init__.py
+++ kfs/__init__.py
@@ -1,1 +1,9 @@
+
+from __future__ import absolute_import
+from . import layers
+from . import constraints
+from . import generators
+from . import objectives
+from . import optimizers
+from . import regularizers
 __version__ = '0.0.1'
