8a5ef93a0e015017043a0fee4e3d624a362dec5e
Michael Oliver <michael.d.oliver@gmail.com>
2017-08-25 10:54:37 -0700

    remove objectives

--- kfs/__init__.py
+++ kfs/__init__.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 from . import layers
 from . import constraints
 from . import generators
-from . import objectives
 from . import optimizers
 from . import regularizers
 __version__ = '0.0.1'
